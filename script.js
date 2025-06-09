f1b4552bca507b8e7ada289e36ee9a39c7cbf1f1
christopher <csuquillol@est.ups.edu.ec>
2025-06-09 16:44:45 -0500

    variables y consolas

--- script.js
+++ script.js
@@ -2,3 +2,23 @@
 function mostrarMensaje(){
     alert("Hola mundo !)");
 }
+
+//variables
+
+function Imprimirvariables(){
+    let nombre = "yo"
+    let edad = "20";
+    console.log("nombre: ", nombre)
+    console.log("edad: ", edad)
+// forma moderna de concatenar cadenas
+    console.log(`nombre:${nombre},edad: ${edad}`)
+    console.error("Error lo que sea")
+    console.warn("Advertencia de triple x")
+}
+
+function arreglos(){
+    let fruta = ['manzana', 'pera', 'banana']
+    for (;et f of frutas){
+        console.log(f)
+    }
+}
